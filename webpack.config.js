a6a3b8f9675e8ce19d26b875e18a180f8e06a860
Gabriela Manzolini <gabi.manzolini@gmail.com>
2019-01-30 10:45:25 -0200

    new branch

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const path = require('path');
+module.exports = {
+    entry: path.join(_dirname, 'src', 'index'),
+    output: {
+        path: path.join(_dirname, 'dist'),
+        filename: 'bundle.js'
+    }
+}
